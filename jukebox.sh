bd8cc2754a814b6c95cac2c019e49519a796f269
corruptNEURON <76706634+corruptNEURON@users.noreply.github.com>
2020-12-28 16:00:59 -0600

    Update jukebox.sh

--- jukebox.sh
+++ jukebox.sh
@@ -17,6 +17,8 @@ while read -ep "Tap: " INPUT; do
 
 	# Trim leading zeros so it doesn't think this is octal or whatever
 	INPUT=$( echo $INPUT | sed 's/^0*//' )
+	
+	echo $INPUT
 
 	# Special case for a toggle card - I hope there are never 1000 plastic cards floating around my house
 	if [[ "$INPUT" -eq "2012737" ]]; then
